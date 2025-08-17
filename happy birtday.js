2464bcc407b56cf2662e0318ff403d6454ec350c
King0717 <kingofgodsu@gmail.com>
2025-08-17 19:48:02 +0530

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -4,7 +4,7 @@ let w = (c.width = window.innerWidth),
   hw = w / 2;
 (hh = h / 2),
   (opts = {
-    strings: ["HAPPIEST", "BIRTHDAY!", "YASHUUUUUUUUUUUUUUUUUUU!!!!!!!", "STAY HAPPY STAY BLESSED", "MAY ALL YOUR DREAMS COME TRUE", "I AM ALWAYS THERE FOR YOU YASHUUUUUU!!!!!!!"],
+    strings: ["HAPPIEST", "BIRTHDAY!", "YASHUUUUUUUU!!!!", "STAY HAPPY STAY BLESSED", "MAY ALL YOUR DREAMS COME TRUE", "I AM ALWAYS THERE FOR YOU YASHUUU!!!!"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
@@ -398,3 +398,4 @@ window.addEventListener("resize", function () {
 
   ctx.font = opts.charSize + "px Verdana";
 });
+
